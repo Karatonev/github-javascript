446b30dc08f306aa97d7ec18774365581d8446d6
Karatonev <h.karatonev@gmail.com>
2024-06-24 16:31:46 +0300

    Add some changes

--- .github/actions/docker-test/test.py
+++ .github/actions/docker-test/test.py
@@ -2,3 +2,6 @@ import os
 print ("Hello World")
 print(os.environ['INPUT_BUCKET_NAME'])
 print(os.environ['INPUT_AWS_REGION'])
+with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
+    print(f'DOCKER_OUTPUT_01=alabala', file=gh_output)
+
